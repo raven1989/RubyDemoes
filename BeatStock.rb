c1b6db8f409181034b4cbf7f230d5de6fa4fe622
raven1989 <okuma0513@yahoo.com>
2016-08-30 23:23:48 +0800

    20160830@home

--- BeatStock.rb
+++ BeatStock.rb
@@ -19,9 +19,44 @@ def beatStock(stocks)
 	return buy,sell
 end
 
-stocks = [2,7,1,3]
+def beatStock1(stocks)
+  #这个要求不能当天买当天卖，必须至少差一天
+  diff = []
+  (1...stocks.size).each { |i| 
+    diff[i-1] = stocks[i]-stocks[i-1]
+  }
+  #puts "#{diff}"
+  # 求和最大子序列
+  buy = sell = 0
+  i = j = 0
+  sum = max_sum = 0
+  while j<diff.size do
+    cur = sum + diff[j]
+    if cur<0 then
+      i = j+1 
+      sum = 0
+    else
+      sum = cur
+      if sum > max_sum then
+        buy = i
+        sell = j
+        max_sum = sum
+      end
+    end
+    j += 1
+  end
+  return buy,sell+1
+end
+
+[
+  [2,7,1,3],
+  [7,6,3,1],
+].each { |stocks|
 buy,sell = beatStock(stocks)
-puts "buy:#{buy} sell:#{sell}"
+puts "beatStocks #{stocks} --> buy:#{buy} sell:#{sell}"
+buy,sell = beatStock1(stocks)
+puts "beatStocks1 #{stocks} --> buy:#{buy} sell:#{sell}"
+}
 
 def maxDistance(seq)
 	i = j = -1
